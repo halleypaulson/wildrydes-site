95aa2fa11cce4aca54c0ebf55c41cfbd10285351
Halley Paulson <paul1098@morris.umn.edu>
2021-04-05 11:57:36 -0500

    Update Config with new ID's

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_NHM2B0c8z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '202tcvo9rmgo1qudb4d0p66gqv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_eyZeGCJhb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '74f9i9v60vdip0b61kd82pk94m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
